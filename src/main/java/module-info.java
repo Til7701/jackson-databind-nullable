61b135d5ad1bd2a4f5931417c54df5950fc8afd0
Tilman Holube <tilman@holube.de>
2025-03-13 23:16:57 +0100

    Added module info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module jackson.databind.nullable {
+    requires com.fasterxml.jackson.databind;
+    requires jakarta.validation;
+    requires java.validation;
+
+    exports org.openapitools.jackson.nullable;
+}

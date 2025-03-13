4e036d76323d35a8ae6f113383ae3ab5e126d12f
Tilman Holube <tilman@holube.de>
2025-03-13 23:37:51 +0100

    removed maybe unneeded javax stuff

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module org.openapitools.jackson.nullable {
     requires com.fasterxml.jackson.databind;
     requires jakarta.validation;
-    requires java.validation;
 
     exports org.openapitools.jackson.nullable;
 }
